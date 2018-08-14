3be58c93c36d9f62cba91cc41c38516c6d4aa0d7
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2018-08-14 19:16:04 +0200

    Init basics

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return ( <
+      div className = "App" >
+      Hi, I Am react <
+      /div>
+    );
+  }
+}
+
+export default App;
